db910d28495e1e176372cb549710f6eed7da0bd0
poryadin.vasiliy <poryadin@gmail.com>
2019-06-15 14:01:22 +0300

    local 1 (eureka / zuul in app.prop)

--- src/main/java/ru/skillbox/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/ru/skillbox/socialnetwork/SocialNetworkApplication.java
@@ -2,8 +2,11 @@ package ru.skillbox.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class SocialNetworkApplication {
 
 	public static void main(String[] args) {

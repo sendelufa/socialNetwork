d07f35c9114b4eba01a05ae12a7e34599deeca59
Ryker <wolkoffanton@gmail.com>
2019-06-20 15:49:15 +0300

    Created AccountService and added registration and setPassword logic

--- src/main/java/ru/skillbox/socialnetwork/service/AccountService.java
+++ src/main/java/ru/skillbox/socialnetwork/service/AccountService.java
@@ -4,17 +4,19 @@ package ru.skillbox.socialnetwork.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.skillbox.socialnetwork.api.request.RegistrationApi;
 import ru.skillbox.socialnetwork.api.request.SetPasswordApi;
-import ru.skillbox.socialnetwork.api.response.AbstractResponse;
 import ru.skillbox.socialnetwork.api.response.ErrorApi;
 import ru.skillbox.socialnetwork.api.response.ErrorDescriptionApi;
 import ru.skillbox.socialnetwork.api.response.ResponseApi;
 import ru.skillbox.socialnetwork.auth.SecurityTokenConfig;
-import ru.skillbox.socialnetwork.dao.PersonDao;
+import ru.skillbox.socialnetwork.dao.PersonDaoService;
 import ru.skillbox.socialnetwork.model.Person;
 import ru.skillbox.socialnetwork.utils.EmailValidator;
 
@@ -23,16 +25,14 @@ import ru.skillbox.socialnetwork.utils.EmailValidator;
 public class AccountService {
 
     @Autowired
-    private PersonDao personDAO;
+    private PersonDaoService personDAO;
     @Autowired
     private SecurityTokenConfig security;
 
 
-    public AbstractResponse registration(RegistrationApi registration) {
+    public ResponseEntity registration(RegistrationApi registration) {
 
-        String userEmail = registration.getEmail();
-
-
+        String userEmail = registration.getEmail(); //TODO добавить валидацию эмейла
 
         if(EmailValidator.isValid(userEmail)) {
             Person person = personDAO.getPersonByEmail(userEmail);
@@ -43,25 +43,24 @@ public class AccountService {
                 person.setEmail(userEmail);
 
                 if (registration.getPasswd1().equals(registration.getPasswd2())) {
-                    //BCryptPasswordEncoder encoder = security.passwordEncoder();
-                    String encodedPassword = security.passwordEncoder().encode(registration.getPasswd1());
+                    BCryptPasswordEncoder encoder = security.passwordEncoder();
+                    String encodedPassword = encoder.encode(registration.getPasswd1());
                     person.setPassword(encodedPassword);
                 } else {
-
-                    return new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"Passwords are not equal"}));
+                    return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"Passwords are not equal"})), HttpStatus.BAD_REQUEST);
                 }
                 personDAO.addPerson(person);
-                return new ResponseApi("string", System.currentTimeMillis(), new ResponseApi.Message("ok"));
+                return new ResponseEntity(new ResponseApi("string", System.currentTimeMillis(), new ResponseApi.Message("ok")), HttpStatus.OK);
 
             } else {
-                return new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"Given email is already used"}));
+                return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"Given email is already used"})), HttpStatus.BAD_REQUEST);
             }
         } else {
-            return new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"Invalid email"}));
+            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"Invalid email"})), HttpStatus.BAD_REQUEST);
         }
     }
 
-    public AbstractResponse setPassword(SetPasswordApi passwordApi){
+    public ResponseEntity setPassword(SetPasswordApi passwordApi){
 
         String password = passwordApi.getPasswdord();
 
@@ -69,6 +68,7 @@ public class AccountService {
             BCryptPasswordEncoder encoder = security.passwordEncoder();
             String encodedPassword = encoder.encode(password);
 
+<<<<<<< HEAD
             Person person  = getCurrentPersonFromSecurityContext();
             person.setPassword(encodedPassword);
 
@@ -76,44 +76,53 @@ public class AccountService {
             return new ResponseApi("string", System.currentTimeMillis(), new ResponseApi.Message("ok"));
         } else {
             return new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"empty string as password"}));
+=======
+            Person person  = (Person) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            person.setPassword(encodedPassword);
+
+            personDAO.updatePerson(person);
+            return new ResponseEntity(new ResponseApi("string", System.currentTimeMillis(), new ResponseApi.Message("ok")), HttpStatus.OK);
+        } else {
+            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"empty string as password"})), HttpStatus.BAD_REQUEST);
+>>>>>>> Created AccountService and added registration and setPassword logic
         }
     }
 
 
-    public AbstractResponse setEmail(String email) {
+<<<<<<< HEAD
+            Person person  = (Person) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            person.setPassword(encodedPassword);
 
-        Person person = getCurrentPersonFromSecurityContext();
-
-        if(person == null){
-            return new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"UNAUTHORIZED"}));
-        } else if (!EmailValidator.isValid(email)){
-            return new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"BAD REQUEST"}));
-        } else {
-            person.setEmail(email);
             personDAO.updatePerson(person);
-            return new ResponseApi("string", System.currentTimeMillis(), new ResponseApi.Message("ok"));
+            return new ResponseEntity(new ResponseApi("string", System.currentTimeMillis(), new ResponseApi.Message("ok")), HttpStatus.OK);
+        } else {
+            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"empty string as password"})), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    public ResponseEntity setEmail(String email) {
+        // тоже из контекста секурити
+
+
+//        person.setEmail(email);
+//        accountService.updatePerson(person);
+
+
+
+        if (true) {
+            return new ResponseEntity(new ResponseApi("string", System.currentTimeMillis(), new ResponseApi.Message("ok")), HttpStatus.OK);
+        } else if (true) {
+            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"string"})), HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"string"})), HttpStatus.UNAUTHORIZED);
         }
     }
 
     public ResponseEntity notification(String notification_type,boolean enable){
 
 
-
-
-
-        if (true){
-            return new ResponseEntity(new ResponseApi("string", System.currentTimeMillis(), new ResponseApi.Message("ok")), HttpStatus.OK);
-        } else if (true){
-            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"string"})), HttpStatus.BAD_REQUEST);
-        } else {
-            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"string"})), HttpStatus.UNAUTHORIZED);
-        }
-    }
-
-
-    public ResponseEntity status(String status){
-
-
+        // Куда мне все это дело сохранить - у персона нет поле нотификаций
 
 
         if (true){
@@ -125,11 +134,14 @@ public class AccountService {
         }
     }
 
+<<<<<<< HEAD
     public Person getCurrentPersonFromSecurityContext(){
         Person person  = (Person) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return person;
     }
 
+=======
+>>>>>>> Created AccountService and added registration and setPassword logic
 
 
 }

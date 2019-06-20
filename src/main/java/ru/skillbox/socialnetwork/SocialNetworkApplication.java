be6fac53cced827c5236089d7a441483a717fd11
Nuclear451 <Koryakovsky.Ivan@gmail.com>
2019-06-20 22:39:50 +0500

    Добавлен первый тест

--- src/main/java/ru/skillbox/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/ru/skillbox/socialnetwork/SocialNetworkApplication.java
@@ -15,16 +15,4 @@ public class SocialNetworkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetworkApplication.class, args);
 	}
-
-    @Bean
-	public ModelMapper modelMapper() {
-		ModelMapper mapper = new ModelMapper();
-		mapper.getConfiguration()
-				.setMatchingStrategy(MatchingStrategies.STRICT)
-				.setFieldMatchingEnabled(true)
-				.setSkipNullEnabled(true)
-				.setFieldAccessLevel(PRIVATE);
-		return mapper;
-	}
-
 }

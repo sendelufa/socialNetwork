4fd3f7052355a1e3858d3d002bbe33fac068c0c3
Ryker <wolkoffanton@gmail.com>
2019-06-20 15:49:15 +0300

    Created AccountService and added registration and setPassword logic

--- src/main/java/ru/skillbox/socialnetwork/controller/AccountController.java
+++ src/main/java/ru/skillbox/socialnetwork/controller/AccountController.java
@@ -13,47 +13,75 @@ import ru.skillbox.socialnetwork.api.request.SetPasswordApi;
 import ru.skillbox.socialnetwork.api.response.ErrorApi;
 import ru.skillbox.socialnetwork.api.response.ErrorDescriptionApi;
 import ru.skillbox.socialnetwork.api.response.ResponseApi;
-import ru.skillbox.socialnetwork.dao.PersonDAO;
-import ru.skillbox.socialnetwork.model.Message;
 import ru.skillbox.socialnetwork.model.Person;
-
-import java.util.List;
+import ru.skillbox.socialnetwork.service.AccountService;
 
 @Controller
 @RequestMapping("/api/v1/account/")
 public class AccountController {
 
     @Autowired
-    PersonDAO personDao;
+    private AccountService accountService;
 
     @RequestMapping(value = "registration", method = RequestMethod.POST)
     public ResponseEntity registration(@RequestBody RegistrationApi registration){
-        return null;
+
+         return accountService.registration(registration);
     }
 
-    @RequestMapping(value = "password/recovery", method = RequestMethod.POST)
+
+    @RequestMapping(value = "password/recovery", method = RequestMethod.PUT)
     public ResponseEntity recoveryPassword(@RequestParam String email){
-        return null;
+
+
+        if (true){
+            return new ResponseEntity(new ResponseApi("string", System.currentTimeMillis(), new ResponseApi.Message("ok")), HttpStatus.OK);
+        } else {
+            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"string"})), HttpStatus.BAD_REQUEST);
+        }
     }
 
-    @RequestMapping(value = "password/set", method = RequestMethod.POST)
+    @RequestMapping(value = "password/set", method = RequestMethod.PUT)
     public ResponseEntity setPassword(@RequestBody SetPasswordApi passwordApi){
-        return null;
+
+        return accountService.setPassword(passwordApi);
     }
 
-    @RequestMapping(value = "email", method = RequestMethod.POST)
-    public ResponseEntity seteEmail(@RequestParam String email){
-        return null;
+    @RequestMapping(value = "email", method = RequestMethod.PUT)
+    public ResponseEntity setEmail(@RequestParam String email){
+        return accountService.setEmail(email);
     }
 
-    @RequestMapping(value = "notification", method = RequestMethod.POST)
+    @RequestMapping(value = "notification", method = RequestMethod.PUT)
     public ResponseEntity notification(@RequestParam String notification_type, @RequestParam boolean enable){
-        return null;
+
+
+        // Что прихоид на вход - новые настройки или...?
+        // Куда мне все это дело сохранить - у персона нет поле нотификаций
+
+
+        if (true){
+            return new ResponseEntity(new ResponseApi("string", System.currentTimeMillis(), new ResponseApi.Message("ok")), HttpStatus.OK);
+        } else if (true){
+            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"string"})), HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"string"})), HttpStatus.UNAUTHORIZED);
+        }
     }
 
-    @RequestMapping(value = "status", method = RequestMethod.POST)
+    @RequestMapping(value = "status", method = RequestMethod.PUT)
     public ResponseEntity status(@RequestParam String status){
-        return null;
+
+
+        // куда мне его установить?
+
+        if (true){
+            return new ResponseEntity(new ResponseApi("string", System.currentTimeMillis(), new ResponseApi.Message("ok")), HttpStatus.OK);
+        } else if (true){
+            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"string"})), HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity(new ErrorApi("invalid_request", new ErrorDescriptionApi(new String[]{"string"})), HttpStatus.UNAUTHORIZED);
+        }
     }
 
 }

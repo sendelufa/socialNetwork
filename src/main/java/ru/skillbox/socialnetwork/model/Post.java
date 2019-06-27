43b9a8c8c5eda58c2e4b2344f296f5b27de81df2
shibkov.k <shibkov.k@gmail.com>
2019-06-27 20:36:14 +0500

    Replaced all Set<T> to List<T>

--- src/main/java/ru/skillbox/socialnetwork/model/Post.java
+++ src/main/java/ru/skillbox/socialnetwork/model/Post.java
@@ -1,7 +1,7 @@
 package ru.skillbox.socialnetwork.model;
 
 import java.util.Date;
-import java.util.Set;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -24,125 +24,125 @@ import javax.validation.constraints.NotNull;
 @Table(name = "post")
 public class Post {
 
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NotNull
-    private int id;
+   @Id
+   @Column(name = "id")
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @NotNull
+   private int id;
 
-     @Column(name = "time")
-    @NotNull
-    private Date time;
+   @Column(name = "time")
+   @NotNull
+   private Date time;
 
-    @ManyToOne
-    @JoinColumn(name = "author_id")
-    @NotNull
-    private Person author;
+   @ManyToOne
+   @JoinColumn(name = "author_id")
+   @NotNull
+   private Person author;
 
-    @Column(name = "title")
-    private String title;
+   @Column(name = "title")
+   private String title;
 
-    @Column(name = "post_text")
-    @NotNull
-    private String postText;
+   @Column(name = "post_text")
+   @NotNull
+   private String postText;
 
-    @Column(name = "is_blocked")
-    @NotNull
-    private boolean isBlocked;
+   @Column(name = "is_blocked")
+   @NotNull
+   private boolean isBlocked;
 
-    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
-    private Set<PostFile> postFiles;
+   @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
+   private List<PostFile> postFiles;
 
-    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
-    @OrderBy("time asc")
-    private Set<PostComment> postComments;
+   @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
+   @OrderBy("time asc")
+   private List<PostComment> postComments;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "post2tag",
-    joinColumns = @JoinColumn(name = "post_id"),
-    inverseJoinColumns = @JoinColumn(name = "tag_id"))
-    private Set<Tag> tags;
+   @ManyToMany(fetch = FetchType.LAZY)
+   @JoinTable(name = "post2tag",
+       joinColumns = @JoinColumn(name = "post_id"),
+       inverseJoinColumns = @JoinColumn(name = "tag_id"))
+   private List<Tag> tags;
 
-    @OneToMany(mappedBy = "person", fetch = FetchType.LAZY)
-    private Set<BlockHistory> blockHistories;
+   @OneToMany(mappedBy = "person", fetch = FetchType.LAZY)
+   private List<BlockHistory> blockHistories;
 
-    public int getId() {
-        return id;
-    }
+   public int getId() {
+      return id;
+   }
 
-    public Person getAuthor() {
-        return author;
-    }
+   public void setId(int id) {
+      this.id = id;
+   }
 
-    public void setAuthor(Person author) {
-        this.author = author;
-    }
+   public Person getAuthor() {
+      return author;
+   }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+   public void setAuthor(Person author) {
+      this.author = author;
+   }
 
-    public Date getTime() {
-        return time;
-    }
+   public Date getTime() {
+      return time;
+   }
 
-    public void setTime(Date time) {
-        this.time = time;
-    }
+   public void setTime(Date time) {
+      this.time = time;
+   }
 
-    public String getTitle() {
-        return title;
-    }
+   public String getTitle() {
+      return title;
+   }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+   public void setTitle(String title) {
+      this.title = title;
+   }
 
-    public String getPostText() {
-        return postText;
-    }
+   public String getPostText() {
+      return postText;
+   }
 
-    public void setPostText(String postText) {
-        this.postText = postText;
-    }
+   public void setPostText(String postText) {
+      this.postText = postText;
+   }
 
-    public boolean isBlocked() {
-        return isBlocked;
-    }
+   public boolean isBlocked() {
+      return isBlocked;
+   }
 
-    public void setBlocked(boolean blocked) {
-        isBlocked = blocked;
-    }
+   public void setBlocked(boolean blocked) {
+      isBlocked = blocked;
+   }
 
-    public Set<PostFile> getPostFiles() {
-        return postFiles;
-    }
+   public List<PostFile> getPostFiles() {
+      return postFiles;
+   }
 
-    public void setPostFiles(Set<PostFile> postFiles) {
-        this.postFiles = postFiles;
-    }
+   public void setPostFiles(List<PostFile> postFiles) {
+      this.postFiles = postFiles;
+   }
 
-    public Set<Tag> getTags() {
-        return tags;
-    }
+   public List<Tag> getTags() {
+      return tags;
+   }
 
-    public void setTags(Set<Tag> tags) {
-        this.tags = tags;
-    }
+   public void setTags(List<Tag> tags) {
+      this.tags = tags;
+   }
 
-    public Set<PostComment> getPostComments() {
-        return postComments;
-    }
+   public List<PostComment> getPostComments() {
+      return postComments;
+   }
 
-    public void setPostComments(Set<PostComment> postComments) {
-        this.postComments = postComments;
-    }
+   public void setPostComments(List<PostComment> postComments) {
+      this.postComments = postComments;
+   }
 
-    public Set<BlockHistory> getBlockHistories() {
-        return blockHistories;
-    }
+   public List<BlockHistory> getBlockHistories() {
+      return blockHistories;
+   }
 
-    public void setBlockHistories(Set<BlockHistory> blockHistories) {
-        this.blockHistories = blockHistories;
-    }
+   public void setBlockHistories(List<BlockHistory> blockHistories) {
+      this.blockHistories = blockHistories;
+   }
 }

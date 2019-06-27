5af58648be42f34350f32af1f1e5bde276edecc0
medweather <medweather@mail.ru>
2019-06-27 14:10:25 +0300

    alter table "notification_settings"

--- src/main/resources/db/migration/V2__FK.sql
+++ src/main/resources/db/migration/V2__FK.sql
@@ -174,3 +174,19 @@ ADD CONSTRAINT `notification_entity_message_fk`
   REFERENCES `message` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
+
+ALTER TABLE `notification_settings`
+ADD INDEX `notification_settings_person_fk_idx` (`person_id` ASC) VISIBLE,
+ADD INDEX `notification_settings_type_fk_idx` (`notification_type_id` ASC) VISIBLE;
+;
+ALTER TABLE `notification_settings`
+ADD CONSTRAINT `notification_settings_person_fk`
+  FOREIGN KEY (`person_id`)
+  REFERENCES `person` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+ADD CONSTRAINT `notification_settings_type_fk`
+  FOREIGN KEY (`notification_type_id`)
+  REFERENCES `notification_type` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;

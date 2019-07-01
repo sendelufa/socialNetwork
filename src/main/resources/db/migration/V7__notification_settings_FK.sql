27416ebef730ed10c0e344a3d2fd8981e7a30ee6
shibkov.k <shibkov.k@gmail.com>
2019-07-01 21:08:58 +0500

    hotfix V7 migration

--- src/main/resources/db/migration/V7__notification_settings_FK.sql
+++ src/main/resources/db/migration/V7__notification_settings_FK.sql
@@ -7,7 +7,7 @@ ADD CONSTRAINT `notification_settings_person_fk`
   FOREIGN KEY (`person_id`)
   REFERENCES `person` (`id`)
   ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `notification_settings_type_fk`
   FOREIGN KEY (`notification_type_id`)
   REFERENCES `notification_type` (`id`)

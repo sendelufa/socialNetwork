1c0a8d3b430295df74389028627db162707d51f5
medweather <medweather@mail.ru>
2019-07-02 18:16:01 +0300

    refactoring tests

--- src/test/java/ru/skillbox/socialnetwork/mapper/mapper/UserMapperTest.java
+++ src/test/java/ru/skillbox/socialnetwork/mapper/mapper/UserMapperTest.java
@@ -13,6 +13,7 @@ import ru.skillbox.socialnetwork.model.enumeration.UserType;
 
 import static org.junit.Assert.assertEquals;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes= AppConfig.class)
 public class UserMapperTest {

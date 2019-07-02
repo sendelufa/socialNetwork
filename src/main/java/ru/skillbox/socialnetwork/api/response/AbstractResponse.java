66825ef2817150670adf07924f5c70c37e80a51d
shibkov.k <shibkov.k@gmail.com>
2019-07-02 21:47:21 +0500

    remove error and timestamp from AbstractResponse

--- src/main/java/ru/skillbox/socialnetwork/api/response/AbstractResponse.java
+++ src/main/java/ru/skillbox/socialnetwork/api/response/AbstractResponse.java
@@ -3,32 +3,16 @@ package ru.skillbox.socialnetwork.api.response;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public abstract class AbstractResponse {
-    @JsonIgnore
-    private boolean isSuccess;
-    private String error;
-    private long timestamp;
 
-    public boolean isSuccess() {
-        return isSuccess;
-    }
+   @JsonIgnore
+   private boolean isSuccess;
 
-    public void setSuccess(boolean success) {
-        isSuccess = success;
-    }
 
-    public String getError() {
-        return error;
-    }
+   public boolean isSuccess() {
+      return isSuccess;
+   }
 
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(long timestamp) {
-        this.timestamp = timestamp;
-    }
+   public void setSuccess(boolean success) {
+      isSuccess = success;
+   }
 }

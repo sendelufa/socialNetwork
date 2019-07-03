3fce74b04fbaa86bbc77403753f050007bd3a14d
poryadin.vasiliy <poryadin@gmail.com>
2019-07-03 15:43:06 +0300

    add login with email & Person req

---
+++ src/main/java/ru/skillbox/socialnetwork/auth/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,15 @@
+package ru.skillbox.socialnetwork.auth;
+
+import org.springframework.security.core.AuthenticationException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationFailureHandler implements org.springframework.security.web.authentication.AuthenticationFailureHandler {
+  @Override
+  public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+    response.sendRedirect("/loginFailed");
+  }
+}

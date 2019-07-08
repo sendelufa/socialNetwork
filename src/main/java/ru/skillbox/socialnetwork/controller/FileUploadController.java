1f03de406e1afeb319e3eac94df9723b3a696421
Alexandr Medvedev <medweather@mail.ru>
2019-07-08 15:29:31 +0300

    changed StorageService and FileUploadController

--- src/main/java/ru/skillbox/socialnetwork/controller/FileUploadController.java
+++ src/main/java/ru/skillbox/socialnetwork/controller/FileUploadController.java
@@ -1,4 +1,29 @@
 package ru.skillbox.socialnetwork.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.skillbox.socialnetwork.service.StorageService;
+
+
+@RestController
+@RequestMapping("/api/v1")
 public class FileUploadController {
+
+    @Autowired
+    private StorageService storageService;
+
+    /**
+     * Загрузка файла в хранилище
+     *
+     * @param type Тип файла
+     */
+    @PostMapping("/storage")
+    public ResponseEntity uploadFile(@RequestParam String type) {
+        //TODO доделать реализацию
+        return null;
+    }
 }
--- src/main/java/ru/skillbox/socialnetwork/service/StorageService.java
+++ src/main/java/ru/skillbox/socialnetwork/service/StorageService.java
@@ -1,4 +1,24 @@
 package ru.skillbox.socialnetwork.service;
 
+import org.springframework.stereotype.Service;
+import ru.skillbox.socialnetwork.api.response.AbstractResponse;
+import ru.skillbox.socialnetwork.api.response.ErrorApi;
+import ru.skillbox.socialnetwork.api.response.FileUploadResponseApi;
+import ru.skillbox.socialnetwork.api.response.ResponseApi;
+
+@Service
 public class StorageService {
+
+    public AbstractResponse uploadFile(String type) {
+        AbstractResponse response;
+
+        if (type.equals("IMAGE")) {
+            response = new ResponseApi("string", System.currentTimeMillis(), new FileUploadResponseApi());
+            response.setSuccess(true);
+            return response;
+        }
+        response = new ErrorApi("invalid request", "Wrong type of entity");
+        response.setSuccess(false);
+        return response;
+    }
 }

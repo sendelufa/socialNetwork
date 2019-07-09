ad70394c6b480b61a2121f889a9f819003c2ecd1
Nuclear451 <Koryakovsky.Ivan@gmail.com>
2019-07-09 07:26:17 +0500

    added import

--- src/main/java/ru/skillbox/socialnetwork/service/AccountService.java
+++ src/main/java/ru/skillbox/socialnetwork/service/AccountService.java
@@ -15,6 +15,7 @@ import ru.skillbox.socialnetwork.model.NotificationSettings;
 import ru.skillbox.socialnetwork.model.Person;
 import ru.skillbox.socialnetwork.model.enumeration.MessagesPermissionPerson;
 import ru.skillbox.socialnetwork.model.enumeration.NameNotificationType;
+import ru.skillbox.socialnetwork.utils.EmailValidator;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;

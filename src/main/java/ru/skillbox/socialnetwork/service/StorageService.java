350d27602c6337596d6a105924501a76f6b93429
Alexandr Medvedev <medweather@mail.ru>
2019-07-09 21:10:01 +0300

    fixed AppConfig

--- src/main/java/ru/skillbox/socialnetwork/service/StorageService.java
+++ src/main/java/ru/skillbox/socialnetwork/service/StorageService.java
@@ -2,34 +2,24 @@ package ru.skillbox.socialnetwork.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import ru.skillbox.socialnetwork.api.response.*;
-
-import java.io.IOException;
+import ru.skillbox.socialnetwork.api.response.AbstractResponse;
+import ru.skillbox.socialnetwork.api.response.ErrorApi;
+import ru.skillbox.socialnetwork.api.response.FileUploadResponseApi;
+import ru.skillbox.socialnetwork.api.response.ResponseApi;
 
 @Service
 public class StorageService {
 
-    public AbstractResponse uploadFile(String type, MultipartFile file) throws IOException {
+    public AbstractResponse uploadFile(String type, MultipartFile file, FileUploadResponseApi fura) {
         AbstractResponse response;
 
-        if (type.equals("IMAGE")) {
-            response = new ResponseApi("string", System.currentTimeMillis(), getDataFile(file));
+        if (type.equals("IMAGE") && file != null) {
+            response = new ResponseApi("string", System.currentTimeMillis(), fura);
             response.setSuccess(true);
             return response;
         }
         response = new ErrorApi("invalid request", "Wrong type of entity");
         response.setSuccess(false);
         return response;
-    }
-
-    public FileUploadResponseApi getDataFile(MultipartFile file) throws IOException {
-        FileUploadResponseApi fura = new FileUploadResponseApi();
-        fura.setOwnerId(new PersonApi().getId());
-        fura.setBytes(file.getBytes().length);
-        fura.setFileFormat(file.getContentType());
-        fura.setFileType(FileUploadResponseApi.fileTypes.IMAGE);
-        fura.setFileName(file.getName());
-        fura.setRawFileURL(file.getOriginalFilename());
-        return fura;
     }
 }

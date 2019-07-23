c44c0f40efb466c3fa7bdcd38fc484257efbe7c6
Alexandr Medvedev <medweather@mail.ru>
2019-07-23 15:42:47 +0300

    delete trash

--- src/main/java/ru/skillbox/socialnetwork/auth/SecurityTokenConfig.java
+++ src/main/java/ru/skillbox/socialnetwork/auth/SecurityTokenConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -53,7 +52,9 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
                             .permitAll()
                     .and()
                         .logout()
-                            .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout")).permitAll();
+                            .logoutUrl("/auth/logout")
+                            .logoutSuccessUrl("/auth/login")
+                            .permitAll();
     }
 
     @Override

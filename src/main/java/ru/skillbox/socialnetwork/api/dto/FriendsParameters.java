c3ee17515047b6ed84fd8465ca21e907221641f5
poryadin.vasiliy <poryadin@gmail.com>
2019-07-27 10:52:41 +0300

    Реализация сервиса - 1 часть

--- src/main/java/ru/skillbox/socialnetwork/api/dto/FriendsParameters.java
+++ src/main/java/ru/skillbox/socialnetwork/api/dto/FriendsParameters.java
@@ -1,8 +1,13 @@
 package ru.skillbox.socialnetwork.api.dto;
 
+import ru.skillbox.socialnetwork.model.Person;
+
 public class FriendsParameters {
   //id текущего пользователя (из БД)
   private int id;
+
+  //собственно сам юзер
+  private Person person;
 
   //имя для поиска
   String name;
@@ -13,21 +18,25 @@ public class FriendsParameters {
   //сдвиг от начала
   private int offset;
 
-
   public FriendsParameters(String name, Integer offset, Integer itemPerPage){
     this.name = name;
     this.offset = offset;
     this.itemPerPage = itemPerPage;
   }
+
   public FriendsParameters(String name, Integer offset){
     this(name, offset, 20);
   }
+
   public FriendsParameters(String name){
     this(name, 0);
   }
 
 
-
+  public void setPerson(Person person) {
+    this.person = person;
+    this.id = person.getId();
+  }
 
   public int getId() {
     return id;
@@ -43,5 +52,9 @@ public class FriendsParameters {
 
   public int getItemPerPage() {
     return itemPerPage;
+  }
+
+  public Person getPerson() {
+    return person;
   }
 }

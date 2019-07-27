4e2211548ddd8ad3ad6c14abc55e5a1b17388875
poryadin.vasiliy <poryadin@gmail.com>
2019-07-27 10:52:41 +0300

    Реализация сервиса - 1 часть

--- src/main/java/ru/skillbox/socialnetwork/dao/FriendsDAO.java
+++ src/main/java/ru/skillbox/socialnetwork/dao/FriendsDAO.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.skillbox.socialnetwork.api.dto.FriendsParameters;
 import ru.skillbox.socialnetwork.api.dto.PersonParameters;
 import ru.skillbox.socialnetwork.model.Friendship;
+import ru.skillbox.socialnetwork.model.Person;
 
 import java.util.List;
 
@@ -24,16 +25,39 @@ public class FriendsDAO {
      return getCurrentSession().createQuery("from Friendship f").list();
   }
 
-  public List<Friendship> searchFriend(FriendsParameters parameters) {
+
+  public List<Friendship> searchAllFriendForPerson(Person person) {
+    String query = "from Friendship f where src_person_id = " + person.getId();
+    List<Friendship> list = getCurrentSession().createQuery(query).list();
+    return list;
+  }
+
+    public List<Friendship> searchFriend(FriendsParameters parameters) {
 
 //    String query = "from Friendship f where src_person_id = " + parameters.getId();
 //
 //    query += parameters.getName().isEmpty() ? "" : " AND f." + parameters.getName();
 //
 
+      return null;
+    }
+
+  public boolean deleteFriendById(int id) {
+    return false;
+  }
+
+  public boolean addPersonAsFriendById(int id) {
+    return false;
+  }
+
+  public List<Friendship> getRequestsByName(FriendsParameters parameters){
+
     return null;
   }
 
+  public List<Friendship> isAFriendOfUsers(int id) {
+    return null;
+  }
 
   private Session getCurrentSession() {
     return sessionFactory.getCurrentSession();
--- src/main/java/ru/skillbox/socialnetwork/service/FriendsService.java
+++ src/main/java/ru/skillbox/socialnetwork/service/FriendsService.java
@@ -3,12 +3,13 @@ package ru.skillbox.socialnetwork.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.skillbox.socialnetwork.api.dto.FriendsParameters;
-import ru.skillbox.socialnetwork.api.response.FriendsApi;
-import ru.skillbox.socialnetwork.api.response.ResponseApi;
+import ru.skillbox.socialnetwork.api.response.*;
 import ru.skillbox.socialnetwork.dao.FriendsDAO;
 import ru.skillbox.socialnetwork.dao.PersonDAO;
 import ru.skillbox.socialnetwork.model.Friendship;
+import ru.skillbox.socialnetwork.model.Person;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,46 +19,106 @@ public class FriendsService {
   private FriendsDAO friendsDAO;
   @Autowired
   private PersonDAO personDAO;
+  @Autowired
+  AccountService accountService;
 
 
-  public ResponseApi searchFriend(FriendsParameters parameters) {
-    List<Friendship> allFriends = friendsDAO.getAllFriendship();
-    allFriends.stream()
-        .filter(e -> e.getDstPerson().getFirstName().indexOf(parameters.getName()) >= 0 || e.getDstPerson().getLastName().indexOf(parameters.getName()) >= 0)
-        .map(this::fillFriendsApi).collect(Collectors.toList());
-    // TODO: 24.07.2019 в процессе
+  public AbstractResponse searchFriend(FriendsParameters parameters) {
+  //FIXME    пока забираем всех друзей пользователя
+    List<Friendship> allFriends = friendsDAO.searchAllFriendForPerson(accountService.getCurrentUser());
+    if(allFriends.size() < 1)
+      return sendError("НетЪ друзей");
+    return mapFriendshipToFriendsApi(allFriends, parameters);
+  }
 
+  public AbstractResponse deleteFriendById(int id) {
+    if(friendsDAO.deleteFriendById(id)){
+      return sendOk();
+    } else {
+      return sendError("Не удалось удалить друга");
+    }
+  }
 
+  public AbstractResponse addPersonAsFriendById(int id) {
+    if(friendsDAO.addPersonAsFriendById(id)){
+      return sendOk();
+    } else {
+      return sendError("Не удалось добавить друга");
+    }
+  }
 
+  public AbstractResponse getRequestsByName(FriendsParameters parameters) {
+    List<Friendship> requests = friendsDAO.getRequestsByName(parameters);
+    if(requests.size() < 1)
+      return sendError("Нет запросов на добавление в друзья!");
+    return mapFriendshipToFriendsApi(requests, parameters);
+  }
 
+  public AbstractResponse getRecommendations() {
+    // FIXME: 27.07.2019 По какому принципу??
     return null;
   }
 
-  public ResponseApi deleteFriendById(int id) {
-    return null;
+  public AbstractResponse isAFriendOfUsers() {
+    List<Friendship> friends = friendsDAO.isAFriendOfUsers(accountService.getCurrentUser().getId());
+    if(friends.size() < 1)
+      return sendError("Это не Ваши друзья");
+    /* FIXME: 27.07.2019 Неверный ответ, надо в таком формате:
+            {
+               "user_ids": [
+                   3
+                ]
+            }
+     */
+    return mapFriendshipToFriendsApi(friends, "");
   }
 
-  public ResponseApi addPersonAsFriendById(int id) {
-    return null;
+  //маппер для PersonApi
+  private PersonApi fillPersonApi(@NotNull Friendship f){
+    PersonApi api = new PersonApi();
+    api.setId(f.getDstPerson().getId());
+    api.setFirst_name(f.getDstPerson().getFirstName());
+    api.setLast_name(f.getDstPerson().getLastName());
+    api.setReg_date(f.getDstPerson().getRegDate().getTime());
+    api.setBirth_date(f.getDstPerson().getBirthDate().getTime());
+    api.setEmail(f.getDstPerson().getEmail());
+    api.setPhone(f.getDstPerson().getPhone());
+    api.setPhoto(f.getDstPerson().getPhoto());
+    api.setAbout(f.getDstPerson().getAbout());
+    //TODO Как получить id города? И ид страны?
+    //api.setTown_id(f.getDstPerson().getTown());
+    api.setMessages_permission(f.getDstPerson().getMessagesPermission());
+    api.setLast_online_time(f.getDstPerson().getLastOnlineTime().getTime());
+    api.setIs_blocked(f.getDstPerson().isBlocked());
+    return api;
   }
 
-  public ResponseApi getRequestsByName(FriendsParameters parameters) {
-    return null;
+  //маппер для FriendsApi
+  private FriendsApi mapFriendshipToFriendsApi(@NotNull List<Friendship> list, FriendsParameters parameters){
+    return mapFriendshipToFriendsApi(list, parameters.getName());
   }
 
-  public ResponseApi getRecommendations() {
-    return null;
-  }
-
-  public ResponseApi isAFriendOfUsers() {
-    return null;
-  }
-
-  private FriendsApi fillFriendsApi(Friendship f){
+  private FriendsApi mapFriendshipToFriendsApi(@NotNull List<Friendship> list, String name){
+    List<PersonApi> apis = list.stream()
+        //FIXME пофиксить - поиск учитывает регистр
+        .filter(e -> e.getDstPerson().getFirstName().indexOf(name) >= 0
+            || e.getDstPerson().getLastName().indexOf(name) >= 0)
+        .map(this::fillPersonApi).collect(Collectors.toList());
     FriendsApi api = new FriendsApi();
-    // TODO: 24.07.2019 в процессе
-
-
-    return null;
+    api.setData(apis);
+    return api;
   }
+
+  private AbstractResponse sendError(String message){
+    AbstractResponse response = new ErrorApi("invalid_request", message);
+    response.setSuccess(false);
+    return response;
+  }
+
+  private AbstractResponse sendOk(){
+    AbstractResponse response = new ResponseApi("none", System.currentTimeMillis(), new ResponseApi.Message("ok"));
+    response.setSuccess(true);
+    return response;
+  }
+
 }
